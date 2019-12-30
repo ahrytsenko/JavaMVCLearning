691a5379d4579d0ea7ed9cebb2782cfc38f21773
agrytsenko <agrytsenko@backbox>
2019-12-30 22:19:49 +0200

    Added an example from https://examples.javacodegeeks.com/core-java/java-swing-mvc-example/

---
+++ src/MVCDemo/Model.java
@@ -0,0 +1,12 @@
+package MVCDemo;
+
+import javax.swing.table.DefaultTableModel;
+
+@SuppressWarnings("serial")
+public class Model extends DefaultTableModel {
+ 
+    public Model() {
+        super(Constants.DATA, Constants.TABLE_HEADER);
+    }
+ 
+}
